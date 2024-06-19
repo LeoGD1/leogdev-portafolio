87739573e49a32b0c14d8e5a8187553e2b56ecf3
LeoGD1 <bridonegd@outlook.com>
2024-06-19 16:33:24 -0600

    proyecto de invitaciones cambiado y pastillas de tecnologias dinamicas

--- scripts/projects.js
+++ scripts/projects.js
@@ -4,25 +4,45 @@ let contenedorProyectos = document.querySelector('.projects__container');
 //PROYECTOS - en este apartado de pueden agregar mas objetos
 const proyectos = [
     {
-        nombre: 'Catálogo de productos.',
+        nombre: 'Portafolio de imprenta.',
         descripcion: 'Este proyecto fué realizado para un negocio dedicado al estampado en diversas superficies con el objetivo de mostrar los productos a los clientes.',
         github: 'https://github.com/LeoGD1/PrintiColorWebPage',
         enlace: 'https://printicolormx.000webhostapp.com/',
+        tecnologias: [
+            'HTML5',
+            'CSS3',
+            'Javascript',
+            'Illustrator'
+        ], 
         imagen: '../images/cap_1.jpg'
     },
     {
-        nombre: 'Menú Digital.',
+        nombre: 'Menú Digital web.',
         descripcion: 'El proyecto fue desarrollado específicamente para un negocio local dedicado a la venta de hamburgesas, snacks y bebidas, tiene como objetivo automatizar el proceso de los pedidos a domicilio.',
         github: 'https://github.com/LeoGD1/menu-kiks-house-burguer',
         enlace: 'https://menu-kiks-house-burguer-codincy.netlify.app/',
+        tecnologias: [
+            'HTML5',
+            'CSS3',
+            'Javascript',
+            'Illustrator'
+        ], 
         imagen: '../images/cap_2.jpg'
 
     },
     {
-        nombre: 'Invitacion Digital Web.',
-        descripcion: 'Este proyecto se trata de un sitio web que funciona como una invitacion digital a un evento, donde los invitados pueden consultar el dia del evento con una cuanta regresiva, además de confirmar asistencia y reproducir una canción especial.',
-        github: 'https://github.com/LeoGD1/Elisa-y-Diego---Nuestra-Boda',
-        enlace: 'https://diegoyelisa-nuestraboda-codincy.netlify.app/ ',
+        nombre: 'Sitio web de invitaciones digitales.',
+        descripcion: 'Este proyecto fue desarrollado para un negocio digital dedicado a la creacion de invitaciones digitales web para cualquier evento, se trata de un emprendimiento iniciado por mi. El sitio web está disponible y es escalable, actualmente continuo implementando diferentes apartados.',
+        github: 'https://github.com/LeoGD1/Condinv-website',
+        enlace: 'https://codinv.netlify.app/',
+        tecnologias: [
+            'HTML5',
+            'CSS3',
+            'Javascript',
+            'React',
+            'Illustrator',
+            'Vite'
+        ], 
         imagen: '../images/cap_3.jpg'
     }
 ];
@@ -30,20 +50,20 @@ const proyectos = [
 //RENDERIZADO DE CADA PROYECTO DEL ARREGLO DE OBJETOS
 proyectos.forEach(proyecto  => {
 
+    let tecnologiasHTML = proyecto.tecnologias.map(tecnologia => {
+        return `<div class="technologie ${tecnologia}">${tecnologia}</div>`;
+    }).join('');
+
     let articulo = `
             <article class="projects__project">
                 <div class="projects__information">
-
                     <div class="projects__container-name">
                         <span class="projects__point animated__point"></span>
                         <h3 class="projects__name">${proyecto.nombre}</h3>
                     </div>
                     <p class="projects__p">${proyecto.descripcion}</p>
                     <div class="projects__technologies">
-                        <div class="technologie HTML5">HTML5</div>
-                        <div class="technologie CSS3">CSS3</div>
-                        <div class="technologie JAVASCRIPT">Javascript</div>
-                        <div class="technologie ILLUSTRATOR">Illustrator</div>
+                        ${tecnologiasHTML}
                     </div>
                     <div class="projects__links">
                         <a href=${proyecto.github} target="_blank" class="projects__link">
